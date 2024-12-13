24091505f6dd05b8164e0b5850b6b5accaa53e1a
Logeshsharma <logeshsharmak@gmail.com>
2024-12-13 13:12:41 +0000

    initial commit

---
+++ app/src/main/java/com/example/androidwithcompose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.androidwithcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.androidwithcompose.ui.theme.AndroidwithComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidwithComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    FirstView(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun FirstView(name: String, modifier: Modifier){
+    Text(
+        text = name,
+        modifier = modifier
+    )
+}
+

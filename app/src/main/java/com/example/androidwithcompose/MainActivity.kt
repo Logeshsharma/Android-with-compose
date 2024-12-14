ecfd449dab72b3f04298b7d43af01d58b7d3e21f
Logeshsharma <logeshsharmak@gmail.com>
2024-12-14 16:08:59 +0000

    Add Greeting text composable view

--- app/src/main/java/com/example/androidwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidwithcompose/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidwithcompose.ui.theme.AndroidwithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,10 +23,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidwithComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    FirstView(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    GreetingText(
+                        modifier = Modifier.padding(10.dp),
+                        message = "Happy birthday",
+                        name = "Logesh sharma"
                     )
                 }
             }
@@ -30,18 +36,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingText(modifier: Modifier = Modifier, message: String, name: String) {
+    Column(modifier = modifier, verticalArrangement = Arrangement.Top) {
+        Text(
+            text = message, fontSize = 24.sp, lineHeight = 100.sp, textAlign = TextAlign.Center
+        )
+        Text(
+            text = name, fontSize = 20.sp, lineHeight = 200.sp, textAlign = TextAlign.Center
+        )
+    }
 }
+
 
 @Composable
-fun FirstView(name: String, modifier: Modifier){
+fun FirstView(name: String, modifier: Modifier) {
     Text(
-        text = name,
-        modifier = modifier
+        text = name, modifier = modifier
     )
 }
-
